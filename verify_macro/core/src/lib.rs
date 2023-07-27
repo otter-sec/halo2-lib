f04cad85c0bff3711056f8df1825e4354e722f4b
qdrs <qdrs@proton.me>
2023-07-27 15:19:18 +0800

    some unit tests

--- verify_macro/core/src/lib.rs
+++ verify_macro/core/src/lib.rs
@@ -1,7 +1,7 @@
 use std::collections::HashSet;
 
 use anyhow::Result;
-use proc_macro2::{TokenStream, Span};
+use proc_macro2::{TokenStream, Span, Ident};
 use quote::{quote, ToTokens};
 
 #[allow(dead_code)]
@@ -28,7 +28,7 @@ fn split_conditions(expr: Box<syn::Expr>) -> Vec<syn::Expr> {
 }
 
 #[allow(dead_code)]
-fn get_unique_variable(expr: Box<syn::Expr>) -> HashSet<syn::Ident> {
+fn get_unique_variable(expr: Box<syn::Expr>) -> HashSet<Ident> {
     match *expr {
         syn::Expr::Binary(b) => {
             let mut left_unique = get_unique_variable(b.left);
@@ -47,6 +47,13 @@ fn get_unique_variable(expr: Box<syn::Expr>) -> HashSet<syn::Ident> {
     }
 }
 
+#[allow(dead_code)]
+fn declare_consts(ident: Ident) -> TokenStream {
+    quote! {
+        let #ident = z3::ast::Int::new_const(&ctx, "#ident");
+    }
+}
+
 pub fn z3_verify(attr: TokenStream, item: TokenStream) -> Result<TokenStream> {
     let _attr = syn::parse2::<syn::ExprBinary>(attr)?;
     let _item = syn::parse2::<syn::ItemFn>(item)?;
@@ -56,23 +63,74 @@ pub fn z3_verify(attr: TokenStream, item: TokenStream) -> Result<TokenStream> {
 
 #[cfg(test)]
 mod tests {
-    use quote::quote;
+    use proc_macro2::Span;
+    use quote::{quote, ToTokens};
 
-    use crate::get_unique_variable;
-
-    // test verify attribute macro
     #[test]
-    fn test_flatten_binaries() {
+    fn test_flatten_binaries_1() {
         let expr = quote! { a == b * c + d && a + b && (a + c || b + d) && !a };
         let expr = Box::new(syn::parse2::<syn::Expr>(expr).unwrap());
         let flattened = super::split_conditions(expr.clone());
-        for f in flattened {
-            println!("{:?}", f);
-        }
+        assert_eq!(flattened.len(), 4);
+        assert_eq!(flattened[0].to_token_stream().to_string(), "a == b * c + d");
+        assert_eq!(flattened[1].to_token_stream().to_string(), "a + b");
+        assert_eq!(flattened[2].to_token_stream().to_string(), "(a + c || b + d)");
+        assert_eq!(flattened[3].to_token_stream().to_string(), "! a");
+    }
 
-        let vars = get_unique_variable(expr);
-        println!("{:?}", vars);
-        // let item = quote! {};
-        // let res = super::z3_verify(attr.into(), item.into()).unwrap();
+    #[test]
+    fn test_flatten_binaries_2() {
+        let expr = quote! { a == b };
+        let expr = Box::new(syn::parse2::<syn::Expr>(expr).unwrap());
+        let flattened = super::split_conditions(expr.clone());
+        assert_eq!(flattened.len(), 1);
+        assert_eq!(flattened[0].to_token_stream().to_string(), "a == b");
+    }
+
+    #[test]
+    fn test_flatten_binaries_3() {
+        let expr = quote! { return };
+        let expr = Box::new(syn::parse2::<syn::Expr>(expr).unwrap());
+        let flattened = super::split_conditions(expr.clone());
+        assert_eq!(flattened.len(), 0);
+    }
+
+    #[test]
+    fn test_flatten_binaries_4() {
+        let expr = quote! { (((a + b))) };
+        let expr = Box::new(syn::parse2::<syn::Expr>(expr).unwrap());
+        let flattened = super::split_conditions(expr.clone());
+        assert_eq!(flattened.len(), 1);
+        assert_eq!(flattened[0].to_token_stream().to_string(), "(((a + b)))");
+    }
+
+    #[test]
+    fn test_unique_variables_1() {
+        let expr = quote! { a == b * c + d && a + b && (a + c || b + d) && !a };
+        let expr = Box::new(syn::parse2::<syn::Expr>(expr).unwrap());
+        let vars = super::get_unique_variable(expr);
+        assert_eq!(vars.len(), 4);
+        assert!(vars.contains(&syn::Ident::new("a", Span::call_site())));
+        assert!(vars.contains(&syn::Ident::new("b", Span::call_site())));
+        assert!(vars.contains(&syn::Ident::new("c", Span::call_site())));
+        assert!(vars.contains(&syn::Ident::new("d", Span::call_site())));
+    }
+
+    #[test]
+    fn test_unique_variables_2() {
+        let expr = quote! { return };
+        let expr = Box::new(syn::parse2::<syn::Expr>(expr).unwrap());
+        let vars = super::get_unique_variable(expr);
+        assert_eq!(vars.len(), 0);
+    }
+
+    #[test]
+    fn test_unique_variables_3() {
+        let expr = quote! { (((a + b))) };
+        let expr = Box::new(syn::parse2::<syn::Expr>(expr).unwrap());
+        let vars = super::get_unique_variable(expr);
+        assert_eq!(vars.len(), 2);
+        assert!(vars.contains(&syn::Ident::new("a", Span::call_site())));
+        assert!(vars.contains(&syn::Ident::new("b", Span::call_site())));
     }
 }
