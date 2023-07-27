bcb796ab13fb036048ca1c3bcf54839a700209b5
qdrs <qdrs@proton.me>
2023-07-27 13:30:39 +0800

    get unique params

--- verify_macro/core/src/lib.rs
+++ verify_macro/core/src/lib.rs
@@ -1,6 +1,8 @@
+use std::collections::HashSet;
+
 use anyhow::Result;
-use proc_macro2::TokenStream;
-use quote::quote;
+use proc_macro2::{TokenStream, Span};
+use quote::{quote, ToTokens};
 
 #[allow(dead_code)]
 // Splits the constraints into a vector of unary, binary or parenthe expressions
@@ -25,6 +27,26 @@ fn split_conditions(expr: Box<syn::Expr>) -> Vec<syn::Expr> {
     }
 }
 
+#[allow(dead_code)]
+fn get_unique_variable(expr: Box<syn::Expr>) -> HashSet<syn::Ident> {
+    match *expr {
+        syn::Expr::Binary(b) => {
+            let mut left_unique = get_unique_variable(b.left);
+            let right_unique = get_unique_variable(b.right);
+            left_unique.extend(right_unique);
+            left_unique
+        },
+        syn::Expr::Paren(p) => get_unique_variable(p.expr),
+        syn::Expr::Path(p) => {
+            let p = p.to_token_stream().to_string();
+            let i = syn::Ident::new(&p, Span::call_site());
+            [i].into()
+        }
+        syn::Expr::Unary(u) => get_unique_variable(u.expr),
+        _ => return [].into()
+    }
+}
+
 pub fn z3_verify(attr: TokenStream, item: TokenStream) -> Result<TokenStream> {
     let _attr = syn::parse2::<syn::ExprBinary>(attr)?;
     let _item = syn::parse2::<syn::ItemFn>(item)?;
@@ -34,17 +56,22 @@ pub fn z3_verify(attr: TokenStream, item: TokenStream) -> Result<TokenStream> {
 
 #[cfg(test)]
 mod tests {
-    use quote::{quote, ToTokens};
+    use quote::quote;
+
+    use crate::get_unique_variable;
 
     // test verify attribute macro
     #[test]
     fn test_flatten_binaries() {
         let expr = quote! { a == b * c + d && a + b && (a + c || b + d) && !a };
         let expr = Box::new(syn::parse2::<syn::Expr>(expr).unwrap());
-        let flattened = super::split_conditions(expr);
+        let flattened = super::split_conditions(expr.clone());
         for f in flattened {
-            println!("{:?}", f.into_token_stream().to_string());
+            println!("{:?}", f);
         }
+
+        let vars = get_unique_variable(expr);
+        println!("{:?}", vars);
         // let item = quote! {};
         // let res = super::z3_verify(attr.into(), item.into()).unwrap();
     }
